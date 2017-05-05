d0f95ef09eb7204ab57189d79717df7e44adb1be
Yoshihiro Tanaka <contact@cordea.jp>
2017-05-05 20:29:02 +0900

    fix test script

--- fcm.sh
+++ fcm.sh
@@ -1,4 +1,4 @@
 curl --header "Authorization: key=$1" \
     --header Content-Type:"application/json" \
     https://fcm.googleapis.com/fcm/send \
-    -d "{\"to\": \"$2\",\"data\": {\"payload\": \"payload here\"}}"
+    -d "{\"to\": \"$2\",\"data\": {\"title\": \"payload here\"}}"
